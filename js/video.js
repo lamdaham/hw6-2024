152c8813390a62b0de0865b723027014a7ee0000
ivanlam <ivanlam@umich.edu>
2024-04-03 20:41:22 -0400

    fixed code

--- js/video.js
+++ js/video.js
@@ -1,11 +1,59 @@
-var video;
+let video = document.getElementById("player1");
 
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
 
 });
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
 
+document.querySelector("#play").addEventListener("click", function() {
+	video.play();
+	console.log(video.volume); 
+	document.getElementById("volume").innerHTML = video.volume * 100 + "%"; 
+});
+
+document.querySelector("#pause").addEventListener("click", function() {
+	video.pause();
+});
+
+document.querySelector("#slower").addEventListener("click", function() {
+	video.playbackRate *= 0.9;
+	console.log(video.playbackRate);
+});
+
+document.querySelector("#faster").addEventListener("click", function() {
+	video.playbackRate /= 0.9;
+	console.log(video.playbackRate);
+});
+
+document.querySelector("#skip").addEventListener("click", function() {
+	if (video.currentTime < video.duration - 10) {
+		video.currentTime += 10;
+	} else {
+		video.currentTime = video.duration;
+	}
+	console.log(video.currentTime);
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+	if (video.muted) {
+		video.muted = false;
+		document.querySelector("#mute").innerHTML = "Mute";
+	} else {
+		video.muted = true;
+		document.querySelector("#mute").innerHTML = "Unmute";
+	}
+});
+
+document.querySelector("#slider").addEventListener("click", function() {
+	video.volume = this.value / 100;
+	document.querySelector("#volume").innerHTML = this.value + "%";
+});
+
+document.querySelector("#vintage").addEventListener("click", function() {
+	video.classList.add("oldSchool");
+});
+
+document.querySelector("#orig").addEventListener("click", function() {
+	video.classList.remove("oldSchool");
+});
